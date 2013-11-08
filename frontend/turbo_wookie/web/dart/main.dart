b33c4ed4d7f91bfb402187c8d0e2ccd9dc7935ce
Michael Peterson <mpeterson4@carthage.edu>
2013-11-08 14:16:39 -0600

    Remove unneed files hopefully?
    This reverts commit e8a1f22674f957c5e346022563ee4f8dcff4780a.
    
    Conflicts:
    	.gitignore
    	frontend/turbo_wookie/.buildlog
    	frontend/turbo_wookie/pubspec.lock
    	frontend/turbo_wookie/pubspec.yaml

--- frontend/turbo_wookie/web/dart/main.dart
+++ frontend/turbo_wookie/web/dart/main.dart
@@ -1,6 +1,27 @@
 import "package:polymer/polymer.dart";
+import "dart:html";
+//import "dart:web_audio";
 
 void main() {
   initPolymer();
-  print("__TURBO WOOKIE__");
+  print("\t\t\t__TURBO WOOKIE__");
+
+  loadData();
+}
+
+void loadData() {
+  String url = "http://shtuff.kuntz.co/deepnote.ogg";
+
+  HttpRequest.getString(url).then(onDataLoaded);
+}
+
+void requestComplete(HttpRequest request) {
+  if(request.status == 200)
+    print("request complete: ${request}");
+  else
+    print("Status Not 200: ${request.status}");
+}
+
+void onDataLoaded(String responseText) {
+  print(responseText);
 }
