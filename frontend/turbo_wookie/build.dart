1c5bb99c4b05ffda3d867be5e26ea6af1a217413
Michael Peterson <mpeterson4@carthage.edu>
2013-11-13 17:11:51 -0600

    Got rid of a warning on the build.dart file.

--- frontend/turbo_wookie/build.dart
+++ frontend/turbo_wookie/build.dart
@@ -1,5 +1,6 @@
 import "package:polymer/builder.dart";
 
-void main() {
-  build(entryPoints: ["web/html/index.html"]);
+void main(List<String> args) {
+  CommandLineOptions options = parseOptions(args);
+  build(entryPoints: ["web/html/index.html"], options: options);
 }
