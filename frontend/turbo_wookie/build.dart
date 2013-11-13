bd2996e64dfcb1bc4207a9d2c87ce2dbff4e0381
Michael Peterson <mpeterson4@carthage.edu>
2013-11-13 17:25:18 -0600

    We should be able to deploy our just to javascript now
    It just doesn't work...

--- frontend/turbo_wookie/build.dart
+++ frontend/turbo_wookie/build.dart
@@ -1,6 +1,6 @@
 import "package:polymer/builder.dart";
 
 void main(List<String> args) {
-  CommandLineOptions options = parseOptions(args);
+  CommandLineOptions options = parseOptions(["--deploy"]);
   build(entryPoints: ["web/html/index.html"], options: options);
 }
