356bd5a024b491916c5c20ffed3ed5146152bc5f
Michael Peterson <mpeterson4@carthage.edu>
2013-11-13 17:38:36 -0600

    Build.dart can use args from the command line now.
    Not that we will ever use that, but whatever...
    
    By the way, to compile our project, right click on build and select run.

--- frontend/turbo_wookie/build.dart
+++ frontend/turbo_wookie/build.dart
@@ -1,6 +1,9 @@
 import "package:polymer/builder.dart";
 
 void main(List<String> args) {
-  CommandLineOptions options = parseOptions(["--deploy"]);
+  List<String> actualArgs = new List<String>();
+  actualArgs.add("--deploy");
+  actualArgs.addAll(args);
+  CommandLineOptions options = parseOptions(actualArgs);
   build(entryPoints: ["web/html/index.html"], options: options);
 }
