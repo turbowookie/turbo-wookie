ef2b71b4edf5cdb664e6a0e461878b47a9ae6104
Don Kuntz <don@dkuntz2.com>
2013-11-16 21:58:48 -0600

    Get a list of all files in the music dir!
    With the one issue of the files currently need to be something that uses
    ID3 tags. So far as I know, only MP3 does that...
    
    If you navigate to /songs, you should get a json array of json objects
    of the songs and some of their ID3 info. The big ones are
    artist/album/name.

--- backend/server.go
+++ backend/server.go
@@ -1,22 +1,37 @@
 package main
 
 import (
-  //"fmt"
   "log"
   "github.com/gorilla/mux"
   "net/http"
   "net/http/httputil"
   "net/url"
-  //"net"
-  //"io"
+  "github.com/fhs/gompd/mpd"
+  "github.com/ascherkus/go-id3/src/id3"
+  "os"
+  "fmt"
+  "encoding/json"
 )
 
+var mpd_conn *mpd.Client
+
 func main() {
+  // connect to MPD
+  conn, err := mpd.Dial("tcp", "localhost:6600")
+  if err != nil {
+    log.Fatal(err)
+  }
+  defer conn.Close()
+  mpd_conn = conn
+
 
   r := mux.NewRouter()
   r.HandleFunc("/stream", 
     httputil.NewSingleHostReverseProxy(&url.URL{Scheme:"http", Host: "localhost:8000", Path: "/"}).ServeHTTP)
+  r.HandleFunc("/songs", listSongs)
 
+
+  // this MUST go last!
   r.PathPrefix("/").Handler(http.FileServer(http.Dir("../frontend/turbo_wookie/web")))
 
   server := &http.Server{
@@ -28,3 +43,26 @@ func main() {
   server.ListenAndServe()
 }
 
+func listSongs(w http.ResponseWriter, r *http.Request) {
+  // get all files from MPD
+  mpdfiles, err := mpd_conn.GetFiles()
+  if err != nil {
+    log.Fatal(err)
+  }
+
+  files := make([]*id3.File, 0)
+  for _, song := range mpdfiles {
+    // grab the file on the filesystem
+    file, err := os.Open("mpd/music/" + song)
+    if err != nil {
+      log.Fatal(err)
+    }
+
+    id3_file := id3.Read(file)
+    files = append(files, id3_file)
+  }
+
+  files_json, err := json.MarshalIndent(files, "", "  ")
+
+  fmt.Fprintf(w, string(files_json))
+}
