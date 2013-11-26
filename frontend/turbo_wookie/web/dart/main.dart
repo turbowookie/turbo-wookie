fe44660082df2290dc0e6207650b7e1954067f77
Michael Peterson <mpeterson4@carthage.edu>
2013-11-25 19:29:58 -0600

    The input takes keyboard input now..
    It's terrible and doesn't work 100% how it should, but it's better then
    ignoring keypresses.

--- frontend/turbo_wookie/web/dart/main.dart
+++ frontend/turbo_wookie/web/dart/main.dart
@@ -1,3 +1,4 @@
+import "dart:convert";
 import "dart:html";
 import "package:polymer/polymer.dart";
 import "media-bar.dart";
@@ -13,8 +14,30 @@ void main() {
   MediaBar mediaBar = querySelector("#mediaBar");
   PlayList playlist = querySelector("#playlist");
   LibraryList library = querySelector("#library");
-  InputElement search = querySelector("#search");
+  TextInputElement search = querySelector("#search");
   mediaBar.setPlayList(playlist);
   library.playlist = playlist;
   search.onInput.listen((Event e) => library.filter(search.value));
+
+  // Terrible hack because for some reason I couldn't get keyboard input
+  // on the input element.
+  search.onKeyDown
+    .where(isGoodKey)
+      .listen((KeyboardEvent e) {
+        String str = ASCII.decode(new List<int>()
+            ..add(e.keyCode));
+        if(!e.shiftKey)
+          str = str.toLowerCase();
+
+        search.value += str;
+
+        library.filter(search.value);
+      });
+}
+
+bool isGoodKey(KeyboardEvent e) {
+  return e.keyCode != KeyCode.BACKSPACE
+      && e.keyCode != KeyCode.LEFT && e.keyCode != KeyCode.RIGHT
+      && e.keyCode != KeyCode.UP && e.keyCode != KeyCode.DOWN
+      && e.keyCode != KeyCode.DELETE;
 }
