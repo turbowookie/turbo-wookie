cf703e3941b173db62498732bf6343a043df92a4
Michael Peterson <mpeterson4@carthage.edu>
2013-11-27 14:00:19 -0600

    We're closer to being able to use this in a real browser.
    It still doesn't work, but now it at least shows our polymer stuff.
    To build it, run `pub build` (you may need to run it on the pubspec.yaml file).
    With the dart editor, you can just right click on `index.html` and select
    `Build Polymer App`. It places all of the compiled down code in the `build`
    folder.

--- frontend/turbo_wookie/build.dart
+++ frontend/turbo_wookie/build.dart
@@ -1,9 +1,5 @@
 import "package:polymer/builder.dart";
 
 void main(List<String> args) {
-  List<String> actualArgs = new List<String>();
-  //actualArgs.add("--deploy");
-  actualArgs.addAll(args);
-  CommandLineOptions options = parseOptions(actualArgs);
-  build(entryPoints: ["web/html/index.html"], options: options);
+  build(entryPoints: ["web/html/index.html"], options: parseOptions(args));
 }
