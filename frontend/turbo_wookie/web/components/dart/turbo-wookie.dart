07b8183dc3ca37dd28aad9925e9bf7396251bb8a
Michael Peterson <mpeterson418@gmail.com>
2014-02-15 14:37:52 -0600

    Added the main Turbo Wookie Polymer element.

---
+++ frontend/turbo_wookie/web/components/dart/turbo-wookie.dart
@@ -0,0 +1,13 @@
+library TurboWookie;
+
+import "package:polymer/polymer.dart";
+
+@CustomTag("tw-turbo-wookie")
+class TurboWookie extends PolymerElement {
+  
+  TurboWookie.created() : super.created();
+  
+  void enteredView() {
+    // Put everything together here.
+  }
+}
