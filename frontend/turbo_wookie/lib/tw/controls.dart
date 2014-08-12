73aef7aab44a8ec2eb1f7c5747a0eed36f603331
Michael Peterson <mpeterson418@gmail.com>
2014-08-12 01:14:17 -0400

    Muting/Unmuting is now a thing. It's also styled nicer now.

--- frontend/turbo_wookie/lib/tw/controls.dart
+++ frontend/turbo_wookie/lib/tw/controls.dart
@@ -1,3 +1,5 @@
+library TurboWookie.Controls;
+
 import "dart:html";
 import "package:polymer/polymer.dart";
 
@@ -6,22 +8,30 @@ import "package:polymer/polymer.dart";
 class Controls extends PolymerElement {
   Controls.created() : super.created();
   
+  static final String PLAY_ICON = "packages/turbo_wookie/tw/images/volume-mute.svg";
+  static final String MUTE_ICON = "packages/turbo_wookie/tw/images/volume.svg";
   @observable String streamSrc;
   double _oldVolume;
   AudioElement audio;
   @observable double volume;
+  @observable String pausePlayIcon;
+  bool isPlaying;
   
   void attached() {
     super.attached();
     
     audio = $["audio"];
     streamSrc = "/stream";
+    isPlaying = true;
     setVolume(vol: 100.0);
+    pausePlayIcon = MUTE_ICON;
     
     audio.onEmptied.listen((e) => reset());
   }
   
   void setVolume({double vol}) {
+    if(!isPlaying) return;
+    
     if(vol != null) {
       volume = vol;
     }
@@ -30,11 +40,22 @@ class Controls extends PolymerElement {
   }
   
   void mute() {
+    isPlaying = false;
     audio.volume = 0.0;
+    pausePlayIcon = PLAY_ICON;
   }
   
   void unmute() {
-    audio.volume = volume;
+    isPlaying = true;
+    setVolume(vol: volume);
+    pausePlayIcon = MUTE_ICON;
+  }
+  
+  void toggleSound() {
+    if(isPlaying)
+      mute();
+    else
+      unmute();
   }
   
   void reset() {
